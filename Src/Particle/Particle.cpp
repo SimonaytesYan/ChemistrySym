63403849462e6d53325590b91dbc509bbae8064b
Simonaytes <yandexman228@gmail.com>
2023-09-22 12:55:18 +0300

    Add collisions particles with walls of flask

--- Src/Particle/Particle.cpp
+++ Src/Particle/Particle.cpp
@@ -5,5 +5,9 @@ void Particle::ChangePosition(Flask* flask)
 {
     position = position + speed * flask->GetTick();
 
-    //TODO Столкновение с границами
+    if (position.GetX() < 0 || flask->GetSize().GetX() < position.GetX())
+        speed = Vector(-speed.GetX(), speed.GetY());
+
+    if (position.GetY() < 0 || flask->GetSize().GetY() < position.GetY())
+        speed = Vector(speed.GetX(), -speed.GetY());
 }
