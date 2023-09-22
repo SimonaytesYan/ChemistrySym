f5d7f8ae5fe2cbc88615816d95092fe7a20d71c4
Simonaytes <yandexman228@gmail.com>
2023-09-22 03:06:28 +0300

    Write main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,8 +4,40 @@
 #include "Particle/CircleParticle/CircleParticle.h"
 #include "Particle/SquareParticle/SquareParticle.h"
 
+const char kWindowHeader[] = "Sphere";
+const int  kWindowSize     = 800;
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(kWindowSize, kWindowSize), 
+                            kWindowHeader);
+
+
     Flask flask(0, 0, 800, 800);
-    flask.AddParticle(CircleParticle(Vector(100, 100), Vector(0, 0), 20));
+
+    flask.AddParticle(new CircleParticle(Vector(100, 100), Vector(0.5, 0.25), 10));
+    flask.AddParticle(new SquareParticle(Vector(120, 120), Vector(0.1, 0.4), 10));
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				case sf::Event::Closed:
+				{
+					window.close();
+				}
+			}
+		}
+
+        window.clear();
+
+        flask.Draw(&window);
+		window.display();
+
+        //int k = 0;
+        //scanf("%c", &k);
+    }
 }
